9e079d1cef98b9eec602ee57bdc4e8b57e8ce3d0
froilainhaeckse <thea_kupler@riseup.net>
2023-07-22 20:43:06 +0200

    add test and functionality for extracting dates

--- scraper.py
+++ scraper.py
@@ -3,12 +3,10 @@
 import os
 import requests
 import locale
+import re
 from bs4 import BeautifulSoup
 from datetime import datetime
 locale.setlocale(locale.LC_TIME, "de_DE") # german
-
-def convert_month_to_number(month_name):
-	return datetime.strptime(month_name, '%B').month
 
 def extract_titles(post_url):
 	# Set source WordPress URL
@@ -33,9 +31,49 @@ def extract_titles(post_url):
 
 	return post_titles
 
-def extract_date(month):
-	# ... (same code as before) ...
-	pass
+def extract_dates(post_url):
+    # Set source WordPress URL
+	wp_url = post_url
+	
+	# Fetch WordPress page HTML
+	response = requests.get(wp_url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+
+	p_tags = soup.find_all('p')
+
+	dates_list = []
+
+	for parent_element in p_tags:
+		# Extract the date from the first welcome post
+		if parent_element == p_tags[0]:
+			date = parent_element.find('span', class_='entry-date').get_text()
+			formatted_date = datetime.strptime(date, "%d. %B %Y").strftime("%Y-%m-%d")
+			dates_list.append(formatted_date)
+		else:
+			# Extract the day from the first <span> element
+			day_element = parent_element.find('span', class_='meta-prep meta-prep-author')
+			if day_element == None:
+				continue
+			if not day_element.get_text().startswith("Veröffentlicht"):
+				continue
+			
+			# Define the regular expression pattern to match the date format, include Mail because of Typo
+			date_pattern = r'\d{1,2}\. ?(?:Januar|Februar|März|April|Mai|Mail|Juni|Juli|August|September|Oktober|November|Dezember) \d{4}'
+			match = re.search(date_pattern, parent_element.get_text())
+			if match:
+				extracted_date = match.group()
+				# Include Mail because of Typo
+				if extracted_date == "09. Mail 2022":
+					extracted_date = "09. Mai 2022"
+				if extracted_date == "02. Mail 2022":
+					extracted_date = "02. Mai 2022"
+				try:
+					date_obj = datetime.strptime(extracted_date, "%d. %B %Y")
+				except ValueError:
+					date_obj = datetime.strptime(extracted_date, "%d.%B %Y")
+				formatted_date = date_obj.strftime("%Y-%m-%d")
+				dates_list.append(formatted_date)
+	return dates_list
 
 def generate_markdown_file(post_date, post_title, post_content, post_category):
 	# ... (same code as before) ...
--- test_scraper.py
+++ test_scraper.py
@@ -1,14 +1,10 @@
 # test_scraper.py
 
 import unittest
-from scraper import convert_month_to_number, extract_titles
+from datetime import datetime
+from scraper import extract_titles, extract_dates
 
 class TestScraper(unittest.TestCase):
-    def test_convert_month_to_number(self):
-        # Test cases for convert_month_to_number function
-        self.assertEqual(convert_month_to_number("Januar"), 1)
-        self.assertEqual(convert_month_to_number("Februar"), 2)
-        self.assertEqual(convert_month_to_number("Dezember"), 12)
 
     def test_extract_titles(self):
         # Test cases for extract_title function
@@ -25,6 +21,30 @@ class TestScraper(unittest.TestCase):
         actual_titles = extract_titles(url)
         for i in range(len(expected_titles)):
             self.assertEqual(actual_titles[i], expected_titles[i])
+    
+    def test_extract_dates(self):
+        # Test cases for extract_title function
+        url = "https://praxisberatung.wordpress.com"
+
+        expected_dates = [
+            "2012-04-02",
+            "2023-06-14",
+            "2023-05-20",
+            "2023-05-08",
+            "2023-04-26"
+        ]
+
+        # print(extract_dates(url))
+        actual_dates = extract_dates(url)
+
+        # Check if the returned value is a list
+        self.assertIsInstance(actual_dates, list)
+
+        # If it's a list of dates, compare each date as datetime objects
+        for i in range(len(expected_dates)):
+            expected_date_obj = datetime.strptime(expected_dates[i], "%Y-%m-%d")
+            actual_date_obj = datetime.strptime(actual_dates[i], "%Y-%m-%d")
+            self.assertEqual(actual_date_obj, expected_date_obj)
 
 if __name__ == "__main__":
     unittest.main()
