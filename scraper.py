d092d6b61ba793764aacc2763ce29246bc687ab8
froilainhaeckse <thea_kupler@riseup.net>
2023-07-23 12:08:54 +0200

    fetching title and date for each post and save it in a dicticionary

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 # scraper.py
 
-import os
 import requests
 import locale
 import re
@@ -8,67 +7,61 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 locale.setlocale(locale.LC_TIME, "de_DE") # german
 
-def extract_titles(html_content):
+def extract_date(date_in_a_string):
+	# Define the regular expression pattern to match the date format, include Mail because of Typo
+	date_pattern = r'\d{1,2}\. ?(?:Januar|Februar|März|April|Mai|Mail|Juni|Juli|August|September|Oktober|November|Dezember) \d{4}'
+	match = re.search(date_pattern, date_in_a_string)
+	if match:
+		extracted_date = match.group()
+		# Include Mail because of Typo
+		if extracted_date == "09. Mail 2022":
+			extracted_date = "09. Mai 2022"
+		if extracted_date == "02. Mail 2022":
+			extracted_date = "02. Mai 2022"
+		try:
+			date_obj = datetime.strptime(extracted_date, "%d. %B %Y")
+		except ValueError:
+			date_obj = datetime.strptime(extracted_date, "%d.%B %Y")
+		formatted_date = date_obj.strftime("%Y-%m-%d")
+		return formatted_date
+
+def extract_data(html_content):
 	h2_tags = html_content.find_all('h2')
-	post_titles = []
+	consolited_data_list = []
+	post_title = ""
+	post_date = ""
 
-	for h2 in h2_tags:
+	for h2_tag in h2_tags:
         # Extract title
-		post_title = h2.get_text().strip()
+		post_title = h2_tag.get_text().strip()
 
-		# Check if the post_title starts with "Mein" or "Herzlich"
-		if not (post_title.startswith("Mein") or post_title.startswith("Herzlich")):
+		# Check if the post_title starts with "Mein" or "Herzlich" or "Froh"
+		if not (post_title.startswith("Mein") or post_title.startswith("Herzlich") or post_title.startswith("Froh")):
 			continue
-	
-		post_titles.append(post_title)
 
-	return post_titles
-
-def extract_dates(html_content):
-	p_tags = html_content.find_all('p')
-
-	dates_list = []
-
-	for parent_element in p_tags:
-		# Extract the date from the first welcome post
-		if parent_element == p_tags[0]:
-			date = parent_element.find('span', class_='entry-date').get_text()
-			formatted_date = datetime.strptime(date, "%d. %B %Y").strftime("%Y-%m-%d")
-			dates_list.append(formatted_date)
+		p_tag_after_h2 = h2_tag.find_next('p')
+		
+		post_date_text = ""
+		if p_tag_after_h2.get_text().startswith("Veröffentlicht"):
+			post_date_text = p_tag_after_h2.get_text()
 		else:
-			# Extract the day from the first <span> element
-			day_element = parent_element.find('span', class_='meta-prep meta-prep-author')
-			if day_element == None:
-				continue
-			if not day_element.get_text().startswith("Veröffentlicht"):
-				continue
-			
-			# Define the regular expression pattern to match the date format, include Mail because of Typo
-			date_pattern = r'\d{1,2}\. ?(?:Januar|Februar|März|April|Mai|Mail|Juni|Juli|August|September|Oktober|November|Dezember) \d{4}'
-			match = re.search(date_pattern, parent_element.get_text())
-			if match:
-				extracted_date = match.group()
-				# Include Mail because of Typo
-				if extracted_date == "09. Mail 2022":
-					extracted_date = "09. Mai 2022"
-				if extracted_date == "02. Mail 2022":
-					extracted_date = "02. Mai 2022"
-				try:
-					date_obj = datetime.strptime(extracted_date, "%d. %B %Y")
-				except ValueError:
-					date_obj = datetime.strptime(extracted_date, "%d.%B %Y")
-				formatted_date = date_obj.strftime("%Y-%m-%d")
-				dates_list.append(formatted_date)
-	return dates_list
+			post_date_text = h2_tag.find_next('div').get_text()
+		if not post_date_text:
+			# Extract the date from the first welcome post
+			post_date_text = html_content.find('p').find('span', class_='entry-date').get_text()
 
-def generate_markdown_files(titles, dates):
+		post_date = extract_date(post_date_text)
+		
+		consolited_data_element = {"title": post_title, "date": post_date}
+		consolited_data_list.append(consolited_data_element)
+
+	return consolited_data_list
+
+def generate_markdown_files(data):
     content_dir = "content/blog/"
-    if len(titles) != len(dates):
-        raise ValueError("Titles and dates lists must have the same count.")
 	
-    # Loop through each title and date at the same time
-    for title, date in zip(titles, dates):
-	    generate_markdown_file(content_dir, title, date)
+    for item in data:
+	    generate_markdown_file(content_dir, item["title"], item["date"])
 
 def generate_markdown_file(content_dir, title, date):
 	# Set destination folder for Hugo content 
@@ -100,6 +93,5 @@ def scrape_wordpress(url):
 if __name__ == "__main__":
     url = "https://praxisberatung.wordpress.com/"
     html_content = scrape_wordpress(url)
-    titles = extract_titles(html_content)
-    dates = extract_dates(html_content)
-    generate_markdown_files(titles, dates)
+    data = extract_data(html_content)
+    generate_markdown_files(data)
